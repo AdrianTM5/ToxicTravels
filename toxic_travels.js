5916911eb1f720d48da6a6dd66819f962fc840ab
AdrianTM5 <angel220803@hotmail.com>
2026-01-26 03:49:49 -0600

    index, principio de css y prueba de btn

---
+++ toxic_travels.js
@@ -0,0 +1,9 @@
+const searchBtn = document.getElementById('searchBtn');
+const clearBtn = document.getElementById('clearBtn');
+
+function oda() {
+    console.log("oda");
+}
+
+searchBtn.addEventListener("click", oda);
+clearBtn.addEventListener("click", oda);
