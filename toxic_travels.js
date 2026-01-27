648a5909e11f734770dcc0c18bccac273dacc3fa
AdrianTM5 <angel220803@hotmail.com>
2026-01-26 21:31:39 -0600

    html finalizados, css casi finalizados y bases de js

--- toxic_travels.js
+++ toxic_travels.js
@@ -1,9 +1,34 @@
 const searchBtn = document.getElementById('searchBtn');
 const clearBtn = document.getElementById('clearBtn');
 
-function oda() {
-    console.log("oda");
+function searchPlace() {
+    debugger;
+    const input = document.getElementById('searchBar').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('api.json')
+    .then(response => response.json())
+    .then(data => {
+        const place = data.places.find(item => item.name.toLowerCase() === input);
+
+        if(place) {
+            const name = place.name.join(', ');
+            const image = place.image.join(', ');
+            const desc = place.desc;
+
+            console.log(`${place.name}`);
+            console.log(`${place.image}`);
+            console.log(`${place.desc}`);
+        } else {
+            console.log('Place not found');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+    });
 }
 
-searchBtn.addEventListener("click", oda);
-clearBtn.addEventListener("click", oda);
+searchBtn.addEventListener("click", searchPlace);
+//clearBtn.addEventListener("click", );
