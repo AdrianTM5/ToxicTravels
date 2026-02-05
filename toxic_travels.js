3bc556392d76535dc0cff2a742f1cbee1d8a1d2a
AdrianTM5 <angel220803@hotmail.com>
2026-02-04 23:47:58 -0600

    se arregló el fetch y mitad de funcionalidad de los botones search y clear

--- toxic_travels.js
+++ toxic_travels.js
@@ -2,7 +2,6 @@ const searchBtn = document.getElementById('searchBtn');
 const clearBtn = document.getElementById('clearBtn');
 
 function searchPlace() {
-    debugger;
     const input = document.getElementById('searchBar').value.toLowerCase();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
@@ -10,9 +9,11 @@ function searchPlace() {
     fetch('api.json')
     .then(response => response.json())
     .then(data => {
-        const place = data.places.find(item => item.name.toLowerCase() === input);
+        
+        const search = data.countries.find(item => item.name.toLowerCase() === input);
+        console.log(search);
 
-        if(place) {
+        /*if(place) {
             const name = place.name.join(', ');
             const image = place.image.join(', ');
             const desc = place.desc;
@@ -21,14 +22,19 @@ function searchPlace() {
             console.log(`${place.image}`);
             console.log(`${place.desc}`);
         } else {
-            console.log('Place not found');
-        }
+            console.log(`${place.desc}`);
+        }*/
     })
     .catch(error => {
         console.error('Error:', error);
         resultDiv.innerHTML = 'An error occurred while fetching data.';
     });
+
 }
 
-searchBtn.addEventListener("click", searchPlace);
-//clearBtn.addEventListener("click", );
+function clearAll() {
+    document.getElementById('searchBar').value = '';
+}
+
+searchBtn.addEventListener("click",searchPlace);
+clearBtn.addEventListener("click", clearAll);
