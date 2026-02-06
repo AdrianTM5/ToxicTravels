a7d4e85c069b601d5fc8aa158ae4d752e0a443b8
AdrianTM5 <angel220803@hotmail.com>
2026-02-06 00:24:50 -0600

    se puede buscar por pais y por ciudad

--- toxic_travels.js
+++ toxic_travels.js
@@ -9,31 +9,55 @@ function searchPlace() {
     fetch('api.json')
     .then(response => response.json())
     .then(data => {
-        
-        const search = data.countries.find(item => item.name.toLowerCase() === input);
-        console.log(search);
-
-        /*if(place) {
-            const name = place.name.join(', ');
-            const image = place.image.join(', ');
-            const desc = place.desc;
-
-            console.log(`${place.name}`);
-            console.log(`${place.image}`);
-            console.log(`${place.desc}`);
-        } else {
-            console.log(`${place.desc}`);
-        }*/
+        switch(input) {
+            case 'sydney': {
+                findCity(data, 'Australia', 'Sydney'); break;
+            }
+            case 'melbourne': {
+                findCity(data, 'Australia', 'Melbourne'); break;
+            }
+            case 'tokyo': {
+                findCity(data, 'Japan', 'Tokyo'); break;
+            }
+            case 'kyoto': {
+                findCity(data, 'Japan', 'Kyoto'); break;
+            }
+            case 'rio de janeiro': {
+                findCity(data, 'Brazil', 'Rio de Janeiro'); break;
+            }
+            case 'sao paulo': {
+                findCity(data, 'Brazil', 'São Paulo'); break;
+            }
+            case 'são paulo': {
+                findCity(data, 'Brazil', 'São Paulo'); break;
+            }
+            default: {
+                var search = data.countries.find(item => item.name.toLowerCase() === input);
+                search.cities.forEach(city => {
+                console.log(city.name);
+                console.log(city.imageUrl);
+                console.log(city.description);
+                });
+                break;
+            }
+        }
     })
     .catch(error => {
         console.error('Error:', error);
         resultDiv.innerHTML = 'An error occurred while fetching data.';
     });
+}
 
+function findCity(data, countryName, cityName) {
+    var search = data.countries.find(c => c.name === countryName).cities.find(city => city.name.includes(cityName));
+    console.log(search.name);
+    console.log(search.imageUrl);
+    console.log(search.description);
 }
 
 function clearAll() {
     document.getElementById('searchBar').value = '';
+    console.clear();
 }
 
 searchBtn.addEventListener("click",searchPlace);
